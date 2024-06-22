de2eda8497a9b0aad437d40e0bdd14aa91ae0e1b
Henvin Joy <henvinjoy122@gmail.com>
2024-06-22 11:55:20 +0530

    created intial spring app

---
+++ Backend/LocalFindr/src/main/java/com/project/localfindr/LocalFindrApplication.java
@@ -0,0 +1,14 @@
+package com.project.localfindr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalFindrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalFindrApplication.class, args);
+		System.out.println("spring app");
+	}
+
+}
---
+++ Backend/LocalFindr/src/test/java/com/project/localfindr/LocalFindrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.localfindr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalFindrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
